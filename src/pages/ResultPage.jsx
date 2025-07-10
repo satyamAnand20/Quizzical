de79def4394bddda4a4f78b91076ae5a192ae330
Satyam Anand <satyamanand2012@gmail.com>
2025-07-10 11:05:30 +0530

    changes to results page.

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -23,20 +23,20 @@ const ResultPage = () => {
   const avgTimePerQ = Math.round(timeSpent / totalQuestions);
 
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://run.confettipage.com/here.js";
-    script.setAttribute(
-      "data-confetticode",
-      "U2FsdGVkX1/eeNHNjewzaXSkKcF8M0QeswlgMLDp6BRtgJxP5AYOjY9PAqeNR/k4p4xMp9aMsEtciR3h6aJIM8VDK3t9w9IUo+NmXzFpIu+6G9D3iaHdb4F4kYuMkJOkZ2JzgoJfFWygFPAC957g9tNK2LolLYLA+k4V0zd9qgU9de/qeDfQWg7+McLCwMP5zYuWXYXqLnKm+Pi04FAk02VLRe3eLiw6+SdaIYSnqTNKysoL0y04xstHHi3LcDxUGEkXHKbBI3kqI7KiOXBCkFG249YodYqNLAUZjm+E1MNPFsHBQU6sHdyND5LR5+Op1WjRT9he7hvUy1+e+Rd6oLYJz5C2/cSOunUX4P/gYm2HIkKqkTDCdsn38GQKDXMdgdcpVth9Y+5z24M/tnniLaUmHRF/1hoMXdy01qkdeoPXV18boTMjR5/m4zdPuP9vGECC1sFQOpSV7vdeYchFg9GCrMjmhP2cIzHQbDEwyqWPnwUzcGIb2P7RjTvSn5B7Uj7yprW9irVBgnikF6p+vsyIBXGHUrsRyN+yWjBi8h9fpHVSLcRIaUJunFXA9/VdWxgUav7MZiX/Doz02MN7mnoe+8aHNz+MOjz0aFncuvEiChuoz8WzSE1eOoUKDWCSirZ0gqjlQN54RG8qABmZZ6pvEHuSloRUvUCwsiwNTFYH6SOoiHTEgvtnfM4JQzwEZAo3BrfyRlvCz+ev5Db9k311Mi4e9hGzY/mJN0EaYFbFPa7rP0QCDK+N1CoaAm7Bba2TgWs7JsOZ5BZkTY0QR4kN/lQvCUgNWlR0j/yWx6E="
-    );
-    script.async = true;
-    document.body.appendChild(script);
+  const script = document.createElement("script");
+  script.src = "https://run.confettipage.com/here.js";
+  script.setAttribute(
+    "data-confetticode",
+    "U2FsdGVkX19nFvHOMTuOp5IX3PTwlc4OOiie+L5mpghQDmNfQkJDEn78u2XQ0F1EQojhrPyogXb8VjNat3c4X/WjY19rxmjiKNlS372fxSfMT5ujRaJCEMwbYV4oZoX4L6A3HfypTPGtvE6E2MvGoDwkoKLrDHUf7rA+A335tlIq72edjx2XccmYSIkXXYsIZidktgOZqKTsgG4IHsDib7ZCvp9kP2Yy6QbIep5myIQqynXg+JAIjMlBNWVo1fm5ppxHSShh27Ex8fcHOpXTKvW+5YEQz+EOARMzHj2HAAbQemMMKSYiERmbhbjYA0fLwOBy+QnwmKMn3EjNFYWYwGtJikQicdQ7+12sX6dSEXjsP9mBvxJjgaIPZ/y6fdxy1u6qyh6svqlv7lPuHOh0iaG6neY2GaQ5JOi1kPFRpRUbXAHQGSbdkKNFUUXvELtbmyXjcZgZo2zjfeKV9hVVWGS+U9WAMWqEu6Zkg8nhi4kwGqWcyr8qUTZUZ1FRHfLc/lD31L8iqdvOY7iPMnnLCspSXykr1yEXkHIs64H4HO09GxHgIWWEFak8aCw833rfXOij1Inby1zrg9Iof6Mb9PYcFr9gFhj7Pe4e+2yEmDlbwqF2hYV1T2s/XhfU9xAmtDm3B6rNvgFL/qIInxex3vkn4VON42JLD8qtNeZO4aCF9n1Jxbqv8Oa1aBMhcgTJaKnF9citPRTBxNHlCVm4/CyX7Yt/s19WGt4zbDhNO+6tDYICw186NCh7lmVOT+RyG+RHvZoLMr7sVAJ1s52tPGsUpgMfYWodcPYLkP8YNzI="
+  );
+  script.async = true;
+  document.body.appendChild(script);
 
-    // Cleanup (optional)
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+  return () => {
+    document.body.removeChild(script);
+  };
+}, []);
+
 
   return (
     <div className="min-h-screen bg-[#102935] text-white flex flex-col items-center py-8 px-4 sm:px-0">

c4cd1fdec777db7c576a6c1a500f82b2ce8f7ed0
Satyam Anand <satyamanand2012@gmail.com>
2025-07-10 11:01:19 +0530

    changes to results display page

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import { useNavigate, useLocation } from "react-router-dom";
 
 const ResultPage = () => {
@@ -20,6 +21,22 @@ const ResultPage = () => {
   const coinEarned = coins || score;
   const accuracy = Math.round((correct / totalQuestions) * 100);
   const avgTimePerQ = Math.round(timeSpent / totalQuestions);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://run.confettipage.com/here.js";
+    script.setAttribute(
+      "data-confetticode",
+      "U2FsdGVkX1/eeNHNjewzaXSkKcF8M0QeswlgMLDp6BRtgJxP5AYOjY9PAqeNR/k4p4xMp9aMsEtciR3h6aJIM8VDK3t9w9IUo+NmXzFpIu+6G9D3iaHdb4F4kYuMkJOkZ2JzgoJfFWygFPAC957g9tNK2LolLYLA+k4V0zd9qgU9de/qeDfQWg7+McLCwMP5zYuWXYXqLnKm+Pi04FAk02VLRe3eLiw6+SdaIYSnqTNKysoL0y04xstHHi3LcDxUGEkXHKbBI3kqI7KiOXBCkFG249YodYqNLAUZjm+E1MNPFsHBQU6sHdyND5LR5+Op1WjRT9he7hvUy1+e+Rd6oLYJz5C2/cSOunUX4P/gYm2HIkKqkTDCdsn38GQKDXMdgdcpVth9Y+5z24M/tnniLaUmHRF/1hoMXdy01qkdeoPXV18boTMjR5/m4zdPuP9vGECC1sFQOpSV7vdeYchFg9GCrMjmhP2cIzHQbDEwyqWPnwUzcGIb2P7RjTvSn5B7Uj7yprW9irVBgnikF6p+vsyIBXGHUrsRyN+yWjBi8h9fpHVSLcRIaUJunFXA9/VdWxgUav7MZiX/Doz02MN7mnoe+8aHNz+MOjz0aFncuvEiChuoz8WzSE1eOoUKDWCSirZ0gqjlQN54RG8qABmZZ6pvEHuSloRUvUCwsiwNTFYH6SOoiHTEgvtnfM4JQzwEZAo3BrfyRlvCz+ev5Db9k311Mi4e9hGzY/mJN0EaYFbFPa7rP0QCDK+N1CoaAm7Bba2TgWs7JsOZ5BZkTY0QR4kN/lQvCUgNWlR0j/yWx6E="
+    );
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Cleanup (optional)
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <div className="min-h-screen bg-[#102935] text-white flex flex-col items-center py-8 px-4 sm:px-0">
